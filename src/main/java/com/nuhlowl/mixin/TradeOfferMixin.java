debd580509cadf8211b7a6dc079621c9046f1118
nuhlowl <nuhlowl@gmail.com>
2024-09-23 16:51:16 -0400

    Removed access widener. Intercepting accessor instead of attempted mutation at tail of constructor

--- src/main/java/com/nuhlowl/mixin/TradeOfferMixin.java
+++ src/main/java/com/nuhlowl/mixin/TradeOfferMixin.java
@@ -1,24 +1,28 @@
 package com.nuhlowl.mixin;
 
+import com.nuhlowl.MiningMagic;
 import net.minecraft.item.ItemStack;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradedItem;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 
 @Mixin(TradeOffer.class)
 public class TradeOfferMixin {
-    @Shadow public boolean rewardingPlayerExperience;
 
-    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/village/TradedItem;Ljava/util/Optional;Lnet/minecraft/item/ItemStack;IIZIIFI)V")
-    public void shouldRewardPlayerExperience(TradedItem firstBuyItem, Optional secondBuyItem, ItemStack sellItem, int _uses, int maxUses, boolean rewardingPlayerExperience, int specialPrice, int demandBonus, float priceMultiplier, int merchantExperience, CallbackInfo ci) {
-        if (sellItem.isEmpty()) {
-            this.rewardingPlayerExperience = false;
+    @Shadow @Final private ItemStack sellItem;
+
+    @Inject(at = @At("HEAD"), method = "shouldRewardPlayerExperience", cancellable = true)
+    public void shouldRewardPlayerExperience(CallbackInfoReturnable<Boolean> cir) {
+        if (this.sellItem.isEmpty()) {
+            cir.setReturnValue(false);
         }
     }
 }
